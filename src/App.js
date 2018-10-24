3306f200b944f7a53e603d0a74f1f15e54a68f38
Gebauer <c9364@M-WNMM-05KJGH5.local>
2018-10-24 16:25:14 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Body from './Body'
+import Nav from './Nav'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  
+
   render() {
     return (
       <div className="App">
+        <Nav />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,6 +26,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Body /> 
       </div>
     );
   }
